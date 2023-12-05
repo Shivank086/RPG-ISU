04648e2ae83147ee91f3d79c0f5ba4f5f551a449
DJFiya <142916623+DJFiya@users.noreply.github.com>
2023-12-05 10:36:35 -0500

    Update Wall.java

--- src/Wall.java
+++ src/Wall.java
@@ -11,29 +11,33 @@ import javax.swing.JFrame;
 
 public class Wall 
 {
-	int x, y, w, l;
+	int x, y, w, l, type;
 	public Wall() {}
-	public Wall(int x, int y, int w, int l)
+	Color[] Colors = {Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN};
+	public Wall(int x, int y, int w, int l, int type)
 	{
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.l = l;
+		this.type = type;
 	}
 	
 	public int getX() {return x;}
 	public int getY() {return y;}
 	public int getW() {return w;}
 	public int getL() {return l;}
+	public int getType() {return type;}
 	public void setX(int x) {this.x = x;}
 	public void setY(int y) {this.y = y;}
 	public void setW(int w) {this.w = w;}
-	public void setL(int l) {this.l = l;}	
+	public void setL(int l) {this.l = l;}
+	public void setType(int type) {this.type = type;}
 	
 	public void paint(Graphics g)
 	{
 		Graphics2D wa = (Graphics2D) g;
-		wa.setColor(Color.RED);
+		wa.setColor(Colors[type-1]);
 		wa.fillRect(getX(), getY(), getW(), getL());
 	}
 }
